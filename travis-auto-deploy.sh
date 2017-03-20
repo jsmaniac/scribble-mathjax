ec4623a80ffedfdf16fc748d6c4ee654c185543d
Georges Dupéron <georges.duperon@gmail.com>
2017-03-20 19:38:24 +0100

    Fixed script.

--- travis-auto-deploy.sh
+++ travis-auto-deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
-set -x
+set +x
 if test "$(git config remote.origin.url)" != "https://github.com/jsmaniac/scribble-mathjax.git"; then
   echo "Not on official repo, will not deploy gh-pages."
 elif test "$TRAVIS_PULL_REQUEST" != "false"; then
   echo "This is a Pull Request, will not deploy gh-pages."
 elif test "$TRAVIS_BRANCH" != "v2.6-racket-mini-source"; then
   echo "Not on v2.6-racket-mini-source branch (TRAVIS_BRANCH = $TRAVIS_BRANCH), will not deploy gh-pages."
-elif test -z "${encrypted_1b66487e02e5_key:-}" -o -z "${encrypted_1b66487e02e5_iv:-}"; then
+elif test -z "${encrypted_675a73236f08_key:-}" -o -z "${encrypted_675a73236f08_iv:-}"; then
   echo "Travis CI secure environment variables are unavailable, will not deploy gh-pages."
 else
+  set -x
   echo "Automatic push to gh-pages"
 
   # Git configuration:
